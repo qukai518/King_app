86f2fd7c6fd6ba7b43e2b3b416807203ed6916f1
qukai518 <85601501+qukai518@users.noreply.github.com>
2022-04-13 12:12:21 +0800

    创建 dyjs_cookies.js

---
+++ APP/dyjs_cookies.js
@@ -0,0 +1,16 @@
+//Cookie和x-Tt-Token去抓到的headers里搜
+let cookies = [
+    {
+        "name": "账号1",
+        "Cookie": "install_id=4209365991041693;ttreq=1$dd05ce5eb139e1fa214a388fe05d87b7970791a4;passport_csrf_token=3f2a27f02ce013b8ac96e2b5cf60dba9;passport_csrf_token_default=3f2a27f02ce013b8ac96e2b5cf60dba9;odin_tt=dc666eca826ab90890a479cc88c3265e144cb8d3b55361f6fe90837bc7a2625cf0f3035d0c3163e669c0969f260032deb5e767d72b79901545982145e550ccd0d0b4b8a7ce8568f4f5290fca69a114d0;n_mh=kCDBxze9oPGf8i6oa5WKcMVhiM683JSna70bsrUJC7A;sid_guard=25e7e9cdf2a978ced442222a4286f1b0%7C1649822476%7C5183999%7CSun%2C+12-Jun-2022+04%3A01%3A15+GMT;uid_tt=098e511d31d05eb33b68625761663542;uid_tt_ss=098e511d31d05eb33b68625761663542;sid_tt=25e7e9cdf2a978ced442222a4286f1b0;sessionid=25e7e9cdf2a978ced442222a4286f1b0;sessionid_ss=25e7e9cdf2a978ced442222a4286f1b0",
+        "x-Tt-Token": "0025e7e9cdf2a978ced442222a4286f1b002dbafa682dc8534217d94c04b2477d2192bc6da2aafa54f54f72edc41da3d63a089fb7d894836923a6174b55ec2cfcd4594cf9950c798115bfd2d8982c9185c8f637143bb65d4cfb19ff5494ed003876ec-1.0.1"
+    },
+    {
+        "name": "账号2",
+        "Cookie": "xxxxxxxxx",
+        "x-Tt-Token": "zzzzzzzzzzzzzzzzzzz"
+    }
+
+];
+
+module.exports = cookies;
